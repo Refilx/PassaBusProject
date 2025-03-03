d8eadf48fd0c49e474f7e270c78bb62861abeb1e
Refilx <jovenestudantex11@gmail.com>
2025-03-03 12:11:55 -0300

    Começo da Criação das tela Pop de seleção de assentos do ônibus

---
+++ src/main/java/br/com/passabus/FXMLPopUpSelecionarAssentosPassageiroController.java
@@ -0,0 +1,17 @@
+package br.com.passabus;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+public class FXMLPopUpSelecionarAssentosPassageiroController {
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    void gridMouseClicked(MouseEvent event) {
+
+    }
+
+}
