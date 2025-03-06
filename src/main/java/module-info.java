e0ba4951d277fb431ea6f5fc0e86c46bbe7021e0
Refilx <jovenestudantex11@gmail.com>
2025-03-06 18:35:56 -0300

    Eliminação das classes de teste Hello e atualização do module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module br.com.passabus {
 
     opens br.com.passabus to javafx.fxml;
     opens br.com.passabus.util to javafx.fxml;
-    exports br.com.passabus;
     exports br.com.passabus.controller;
     opens br.com.passabus.controller to javafx.fxml;
     exports br.com.passabus.model.aplication;
