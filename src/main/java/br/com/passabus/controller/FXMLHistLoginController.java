68e13fd4dd9af5e568d4134ae2a624619593feaf
Refilx <jovenestudantex11@gmail.com>
2025-03-13 19:09:33 -0300

    "Finalização" da classes de histórico de venda e de login com a configuração das tabelas com os dados do banco de dados

--- src/main/java/br/com/passabus/controller/FXMLHistLoginController.java
+++ src/main/java/br/com/passabus/controller/FXMLHistLoginController.java
@@ -1,24 +1,54 @@
 package br.com.passabus.controller;
 
+import br.com.passabus.model.dao.LogDAO;
+import br.com.passabus.model.dao.ViagemDAO;
+import br.com.passabus.model.domain.Log;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class FXMLHistLoginController {
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TableColumn<?, ?> tableColumnData;
-
-    @FXML
-    private TableColumn<?, ?> tableColumnLogin;
-
-    @FXML
-    private TableColumn<?, ?> tableColumnLogout;
+public class FXMLHistLoginController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> tableColumnUsername;
+    private TableColumn<?, ?> tc_login;
 
     @FXML
-    private TableView<?> tableViewLogin;
+    private TableColumn<?, ?> tc_logout;
 
+    @FXML
+    private TableColumn<?, ?> tc_username;
+
+    @FXML
+    private TableView<?> tvHistLogin;
+
+    private ObservableList observableList;
+    private LinkedList<Log> listaDeLogs = new LinkedList<>();
+
+    void prepararListaTabela() {
+        tc_login.setCellValueFactory(new PropertyValueFactory<>("dtLogin"));
+        tc_logout.setCellValueFactory(new PropertyValueFactory<>("dtLogout"));
+        tc_username.setCellValueFactory(new PropertyValueFactory<>("username"));
+
+        // Pegando a lista de viagens do banco
+        listaDeLogs =  new LogDAO().getLogs();
+
+        // configurando o observable list com os dados da lista do banco
+        observableList = FXCollections.observableList(listaDeLogs);
+
+        // Configurando a tabela após a pesquisa
+        tvHistLogin.setItems(observableList);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        prepararListaTabela();
+    }
 }
--- src/main/java/br/com/passabus/controller/FXMLHistVendaController.java
+++ src/main/java/br/com/passabus/controller/FXMLHistVendaController.java
@@ -1,28 +1,62 @@
 package br.com.passabus.controller;
 
+import br.com.passabus.model.dao.VendaDAO;
+import br.com.passabus.model.domain.Venda;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class FXMLHistVendaController {
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TableColumn<?, ?> tableColumnData;
+public class FXMLHistVendaController implements Initializable {
 
-    @FXML
-    private TableColumn<?, ?> tableColumnDestino;
-
-    @FXML
-    private TableColumn<?, ?> tableColumnOrigem;
 
     @FXML
-    private TableColumn<?, ?> tableColumnValor;
+    private TableColumn<?, ?> tc_data;
 
+    @FXML
+    private TableColumn<?, ?> tc_bilhete;
+
+    @FXML
+    private TableColumn<?, ?> tc_status;
+
+    @FXML
+    private TableColumn<?, ?> tc_formaPagemento;
+
+    @FXML
+    private TableColumn<?, ?> tc_valor;
     @FXML
     private TableView<?> tableViewVendas;
 
-    void prepararListaTabela() {
+    private ObservableList observableList;
+    private LinkedList<Venda> listaDeVendas = new LinkedList<>();
 
+    void prepararListaTabela() {
+        tc_data.setCellValueFactory(new PropertyValueFactory<>("dtVenda"));
+        tc_valor.setCellValueFactory(new PropertyValueFactory<>("valorTotal"));
+        tc_bilhete.setCellValueFactory(new PropertyValueFactory<>("bilhete"));
+        tc_status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        tc_formaPagemento.setCellValueFactory(new PropertyValueFactory<>("opcaoPagamento"));
+
+        // Pegando a lista de viagens do banco
+        listaDeVendas =  new VendaDAO().getVendas();
+
+        // configurando o observable list com os dados da lista do banco
+        observableList = FXCollections.observableList(listaDeVendas);
+
+        // Configurando a tabela após a pesquisa
+        tableViewVendas.setItems(observableList);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        prepararListaTabela();
     }
 
 }
