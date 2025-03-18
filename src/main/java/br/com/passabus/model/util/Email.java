a9e6647e56305c235f0806764d8d54d9038d6790
Refilx <jovenestudantex11@gmail.com>
2025-03-17 23:44:03 -0300

    Adição de classe que será usada para o envio de email via aplicação para o email de usuário cadastrados para a recuperação de senha usadando um gerador de código para a chave a ser inserida pelo usuário  e adição do ícone e usuário

---
+++ src/main/java/br/com/passabus/model/util/Email.java
@@ -0,0 +1,52 @@
+package br.com.passabus.model.util;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.SimpleEmail;
+
+import javax.swing.*;
+import java.util.UUID;
+
+public class Email {
+
+    public void sendMailTo(String destinatario) {
+        // Dependências necessárias: commons-email-1.5 e mail-1.6.2
+
+        String[] code = generateUniqueCode().split("-");
+        String recoverycode = code[1]+"-"+code[2]+"-"+code[3];
+
+        String msg = "Recebemos uma solicitação para recuperar a senha da sua conta.\n" +
+                "Favor usar o código %s somente válido enquanto a tela de inserção do código estiver aberta.\n".formatted(recoverycode) +
+                "Se você não solicitou a recuperação da sua senha, favor ignorar essa mensagem.\n\n" +
+                "---------------\n" +
+                "PassaBus Administration";
+
+        String appMail = "passabus.official@gmail.com";
+        String appMailPass = "akql uukw epmm qosn"; // Senha de app gerada pelo google
+
+        SimpleEmail email = new SimpleEmail();
+        email.setHostName("smtp.gmail.com");
+        email.setSmtpPort(587);  // Porta 587 para STARTTLS
+        email.setSSLOnConnect(false);  // Desabilitar SSL direto
+        email.setStartTLSEnabled(true); // Habilitar STARTTLS
+        email.setAuthenticator(new DefaultAuthenticator(appMail, appMailPass));
+
+        try {
+            email.setFrom(appMail);
+            email.setSubject("Recuperação de senha | PassaBus | Não Responda");
+            email.setMsg(msg);
+            email.addTo(destinatario);
+            email.send();
+            JOptionPane.showMessageDialog(null, "Email enviado com sucesso!\nFavor confira sua caixa de entrada!",
+                    "E-mail de recuperação de senha enviado", JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Gerar um código único
+    public static String generateUniqueCode() {
+        UUID uniqueKey = UUID.randomUUID(); // Gera um UUID aleatório
+        return uniqueKey.toString(); // Retorna o código como uma string
+    }
+}
